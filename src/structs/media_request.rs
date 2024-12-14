bb22c96e26b77aae2bfa8901206c8675358284c0
Verity <83372423+VerityIncorporated@users.noreply.github.com>
2024-12-14 20:36:36 +0000

    Update media_request.rs
    No longer serializes fields which are 'None', making requests actually go through.

--- src/structs/media_request.rs
+++ src/structs/media_request.rs
@@ -3,30 +3,30 @@ use serde::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize, Default, Debug)]
 pub struct MediaRequestData {
     pub url: String,
-    #[serde(rename = "videoQuality")]
+    #[serde(rename = "videoQuality", skip_serializing_if = "Option::is_none")]
     pub video_quality: Option<String>,
-    #[serde(rename = "audioFormat")]
+    #[serde(rename = "audioFormat", skip_serializing_if = "Option::is_none")]
     pub audio_format: Option<String>,
-    #[serde(rename = "audioBitrate")]
+    #[serde(rename = "audioBitrate", skip_serializing_if = "Option::is_none")]
     pub audio_bitrate: Option<String>,
-    #[serde(rename = "filenameStyle")]
+    #[serde(rename = "filenameStyle", skip_serializing_if = "Option::is_none")]
     pub filename_style: Option<String>,
-    #[serde(rename = "downloadMode")]
+    #[serde(rename = "downloadMode", skip_serializing_if = "Option::is_none")]
     pub download_mode: Option<String>,
-    #[serde(rename = "youtubeVideoCodec")]
+    #[serde(rename = "youtubeVideoCodec", skip_serializing_if = "Option::is_none")]
     pub youtube_video_codec: Option<String>,
-    #[serde(rename = "youtubeDubLang")]
+    #[serde(rename = "youtubeDubLang", skip_serializing_if = "Option::is_none")]
     pub youtube_dub_lang: Option<String>,
-    #[serde(rename = "alwaysProxy")]
+    #[serde(rename = "alwaysProxy", skip_serializing_if = "Option::is_none")]
     pub always_proxy: Option<bool>,
-    #[serde(rename = "disableMetadata")]
+    #[serde(rename = "disableMetadata", skip_serializing_if = "Option::is_none")]
     pub disable_metadata: Option<bool>,
-    #[serde(rename = "tiktokFullAudio")]
+    #[serde(rename = "tiktokFullAudio", skip_serializing_if = "Option::is_none")]
     pub tiktok_full_audio: Option<bool>,
-    #[serde(rename = "tiktokH265")]
+    #[serde(rename = "tiktokH265", skip_serializing_if = "Option::is_none")]
     pub tiktok_h265: Option<bool>,
-    #[serde(rename = "twitterGif")]
+    #[serde(rename = "twitterGif", skip_serializing_if = "Option::is_none")]
     pub twitter_gif: Option<bool>,
-    #[serde(rename = "youtubeHLS")]
-    pub youtube_hls: Option<bool>
+    #[serde(rename = "youtubeHLS", skip_serializing_if = "Option::is_none")]
+    pub youtube_hls: Option<bool>,
 }
