ae5d2dd163f713d784a87be764135dbfd8b2ad72
Verity <83372423+VerityIncorporated@users.noreply.github.com>
2024-12-17 15:58:15 +0000

    we don't need this, right guys?

--- src/api/client.rs
+++ src/api/client.rs
@@ -1,6 +1,6 @@
 use futures_util::StreamExt;
 use once_cell::sync::Lazy;
-use reqwest::{header::CONTENT_LENGTH, Client as ReqwestClient};
+use reqwest::Client as ReqwestClient;
 use std::{env, fs::File, io::Write, sync::Arc};
 use tokio::sync::RwLock;
 
@@ -207,18 +207,6 @@ impl Client {
         path: String,
     ) -> Result<(), Box<dyn std::error::Error>> {
         let response = reqwest::get(&tunnel_link).await?;
-    
-        if let Some(content_length) = response.headers().get(CONTENT_LENGTH) {
-            let content_length = content_length.to_str()?.parse::<u64>()?;
-    
-            if content_length == 0 {
-                eprintln!("The file has a content length of 0 bytes. Something went wrong.");
-                return Err("File has a content length of 0 bytes.".into());
-            }
-        } else {
-            eprintln!("Content-Length header is missing. Something went wrong.");
-            return Err("Content-Length header is missing.".into());
-        }
     
         if !response.status().is_success() {
             eprintln!("Failed to download file: HTTP {}", response.status());
